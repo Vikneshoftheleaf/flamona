7129144cd83b798fc927450bcbad68d14c117302
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-12-25 17:51:41 +0530

    google-analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-import { GoogleAnalytics } from '@next/third-parties/google'
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
@@ -11,8 +9,16 @@ export default function RootLayout({ children }) {
         <script src="https://kit.fontawesome.com/067fc88846.js" crossorigin="anonymous"></script>
       </head>
       <body className={inter.className}>
+        <Script id="next" async src="https://www.googletagmanager.com/gtag/js?id=G-WNKR8EHTC1"></Script>
+        <Script id="next">
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WNKR8EHTC1');
+        </Script>
         <main>
-        {children}
+          {children}
         </main>
       </body>
       <GoogleAnalytics gaId="G-WNKR8EHTC1" />
