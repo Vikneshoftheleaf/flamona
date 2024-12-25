080d3db164f0f2a0114d2c1eee8b1f4b30a3fd81
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-12-25 17:42:29 +0530

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,14 +7,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script src="https://kit.fontawesome.com/067fc88846.js" crossorigin="anonymous"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WNKR8EHTC1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-WNKR8EHTC1');
-        </script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
