1c7194d4850568fba5b86beb43bb772065c28cbd
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-12-25 17:54:10 +0530

    revert

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
@@ -9,14 +8,6 @@ export default function RootLayout({ children }) {
         <script src="https://kit.fontawesome.com/067fc88846.js" crossorigin="anonymous"></script>
       </head>
       <body className={inter.className}>
-        <Script id="next" async src="https://www.googletagmanager.com/gtag/js?id=G-WNKR8EHTC1"></Script>
-        <Script id="next">
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-WNKR8EHTC1');
-        </Script>
         <main>
           {children}
         </main>
